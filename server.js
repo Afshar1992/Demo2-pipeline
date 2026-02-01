60907312b52b380ea0fff07a761ece5c92f617d9
Naqib Afshar <naqibafshar57@gmail.com>
2026-02-01 20:45:32 +0300

    dummy commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 app.get('/', (req, res) => {
     res.json({
         status: 200,
-        message: "hello world from all cities of Italy"
+        message: "hello world from all cities of sudan"
     })
 })
 
